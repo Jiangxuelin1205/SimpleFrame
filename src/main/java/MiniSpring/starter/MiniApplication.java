8054bd945777a2aa2731176d5e75ff31791bf1b5
Jiangxuelin1205 <Jiangxuelin1205@163.com>
2019-06-15 15:08:41 +0800

    tomcat imbeded

---
+++ src/main/java/MiniSpring/starter/MiniApplication.java
@@ -0,0 +1,17 @@
+package MiniSpring.starter;
+
+import MiniSpring.web.server.TomcatServer;
+import org.apache.catalina.LifecycleException;
+
+public class MiniApplication {
+
+    public static void run(Class<?> cls,String[] args){
+        System.out.println("Hello mini-spring");
+        TomcatServer tomcatServer=new TomcatServer(args);
+        try {
+            tomcatServer.startServer();
+        } catch (LifecycleException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/MiniSpring/web/server/TomcatServer.java
@@ -0,0 +1,45 @@
+package MiniSpring.web.server;
+
+import MiniSpring.web.servlet.TestServlet;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.startup.Tomcat;
+
+public class TomcatServer {
+
+    private Tomcat tomcat;
+    private String[] args;
+
+    public TomcatServer(String[] args) {
+        this.args = args;
+    }
+
+    public void startServer() throws LifecycleException {
+        tomcat = new Tomcat();
+        tomcat.setPort(6699);
+        tomcat.start();
+
+        Context context=new StandardContext();
+        context.setPath("");
+        context.addLifecycleListener(new Tomcat.FixContextListener());
+
+        TestServlet servlet=new TestServlet();
+        Tomcat.addServlet(context,"testServlet",servlet).setAsyncSupported(true);
+        context.addServletMappingDecoded("/test.json","testServlet");
+
+        tomcat.getHost().addChild(context);
+
+
+
+        Thread awaitThread = new Thread("await") {
+            @Override
+            public void run() {
+                TomcatServer.this.tomcat.getServer().await();
+            }
+        };
+        awaitThread.setDaemon(false);
+        awaitThread.start();
+
+    }
+}
